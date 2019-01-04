6a84d48338a89a653d94c366d8c727d4acb000e3
Hunter Fabrizzio <hfabrizzio@ucvts.org>
2019-01-04 10:47:26 -0500

    almost finished calculation

--- js.js
+++ js.js
@@ -41,19 +41,59 @@ else if (queuedOperation = 4) {
 
 //////////////////////////////////////
 
-if (funct==add) {
+if (funct=="add") {
   operation = 1
 }
-else if (funct==subtract) {
+else if (funct=="subtract") {
   operation = 2
 }
-else if (funct==multiply) {
+else if (funct=="multiply") {
   operation = 3
 }
-else if (funct==divide) {
+else if (funct=="divide") {
   operation = 4
 }
 
 queuedOperation = operation
+displayNum = " "
+display.value = displayNum
+}
+
+function calculation() {
+  var display = document.getElementById("display")
+  displayNum = display.value
+    evalDisplay = eval(displayNum);
+    evalStored = eval(storedNum);
+
+if (operation = 1) {
+  storedNum = evalStored + evalDisplay
+}
+else if (operation = 2) {
+  storedNum = evalStored - evalDisplay
+}
+else if (operation = 3) {
+  storedNum = evalStored * evalDisplay
+}
+else if (operation = 4) {
+  storedNum = evalStored / evalDisplay
+}
+
+eval(storedNum) = storedNum
+display.value = storedNum
+
+//////////////////////////////////////////////
+
+if (operation != 0) {
+  finsihedCalculation = true
+}
+
+/////////////////////////////////////////////
+
+operation = 0;
+queuedOperation = 0;
+displayNum = "";
+storedNum = "";
+
+
 
 }
